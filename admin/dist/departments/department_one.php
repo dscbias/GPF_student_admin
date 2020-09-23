b9bdd76ccf31c329dbe488411d5851ff009c2c53
Sumit Chauhan <45000045+codeit13@users.noreply.github.com>
2020-09-23 18:49:13 +0530

    Update department_one.php

--- admin/dist/departments/department_one.php
+++ admin/dist/departments/department_one.php
@@ -39,13 +39,11 @@
             </ol>
               <div class="card mb-4">
                   <div class="card-body">
-                      <p class="mb-0">
-                          This page is an example of using static navigation. By removing the
-                          <code>.sb-nav-fixed</code>
-                          class from the
-                          <code>body</code>
-                          , the top navigation and side navigation will become static on scroll. Scroll down this page to see an example.
-                      </p>
+                    
+                    
+        <!--          Write your Code here....              -->
+                    
+                    
                   </div>
               </div>
               <div style="height: 100vh;"></div>
