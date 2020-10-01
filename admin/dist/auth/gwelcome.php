9fc0f7799871180181f366ca2d0482dba51e6c12
GARIMA-JEENA <69374495+GARIMA-JEENA@users.noreply.github.com>
2020-10-01 16:50:32 +0530

    Update gwelcome.php

--- admin/dist/auth/gwelcome.php
+++ admin/dist/auth/gwelcome.php
@@ -21,7 +21,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 </head>
 <body>
     <div class="page-header">
-        <h1>Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.</h1>
+        <h1>Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to GPF.</h1>
     </div>
     <p>
         <a href="gpassword_reset.php" class="btn btn-warning">Reset Your Password</a>
